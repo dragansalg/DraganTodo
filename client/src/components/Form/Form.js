959abf89e0092adb72250896608d62707cee2ccb
Dragan Sälg <dragan.salg@gmail.com>
2021-11-24 15:02:12 +0100

    add function to send data to server

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper} from '@material-ui/core';
+import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux'
 
 import useStyles from './styles';
+import { createPost} from '../../actions/posts'
 
 const Form = () => {
+    const [postData, setPostData] = useState({ title: '', message: '', selectedFile: '' })
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(createPost(postData));
+    }
+
+    const clear = () => {
+
+    }
+
     return (
-        <>
-        <h1>FORM</h1>
-        </>
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Typography varian="h6">Creating a Memory</Typography>
+                <TextField name="title"variant="outlined"label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
+                <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
+                <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({base64}) => setPostData({...postData, selectedFile: base64})} /></div>
+                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+                <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
+            </form>
+        </Paper>
     );
 }
 
